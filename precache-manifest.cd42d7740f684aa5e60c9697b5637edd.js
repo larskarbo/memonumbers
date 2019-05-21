58d8aea2e10e35779d9820dfca9632fa28d4644a
Lars Karbo <mail@larskarbo.no>
2019-05-21 23:20:25 +0200

    Updates

---
+++ precache-manifest.cd42d7740f684aa5e60c9697b5637edd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece1166a4c7ed28713e87c9e6c5fda23",
+    "url": "/memonumbers/index.html"
+  },
+  {
+    "revision": "b89f62322fa88ce31cc1",
+    "url": "/memonumbers/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/memonumbers/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "b89f62322fa88ce31cc1",
+    "url": "/memonumbers/static/js/main.abf92733.chunk.js"
+  },
+  {
+    "revision": "8f83e411a610b75022b9",
+    "url": "/memonumbers/static/js/runtime~main.928b6453.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/memonumbers/static/media/logo.5d5d9eef.svg"
+  }
+]);
